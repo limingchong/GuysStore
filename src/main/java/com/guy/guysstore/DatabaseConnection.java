c2da6f852e360131b6cca1bae7014921548581cb
LmcJames <19726006@bjtu.edu.cn>
2022-11-09 21:16:51 +0800

    connect remote server

--- src/main/java/com/guy/guysstore/DatabaseConnection.java
+++ src/main/java/com/guy/guysstore/DatabaseConnection.java
@@ -10,9 +10,9 @@ public class DatabaseConnection {
     //Creating Connection
     public static Connection connection;
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "1022";
-    public static final String URL = "jdbc:mysql://localhost:3306/storedata?autoReconnect=true&useSSL=false";
+    public static final String USERNAME = "group5";
+    public static final String PASSWORD = "BJTUgroup5";
+    public static final String URL = "jdbc:mysql://rm-8vbr8ca021o44ca4euo.mysql.zhangbei.rds.aliyuncs.com:3306/store?autoReconnect=true&useSSL=false";
 
     public DatabaseConnection() {
         try {
