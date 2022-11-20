c24fec15b2a49bb0b5b8e0ff62f6a774a137d9c6
LmcJames <19726006@bjtu.edu.cn>
2022-11-20 16:11:27 +0800

    encry decry

--- src/main/java/com/guy/restful/LogIn.java
+++ src/main/java/com/guy/restful/LogIn.java
@@ -1,5 +1,6 @@
 package com.guy.restful;
 
+import com.guy.guysstore.CryptoSystemClient;
 import com.guy.guysstore.DatabaseConnection;
 import com.guy.guysstore.CryptoSystemServer;
 
@@ -11,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "/login")
 public class LogIn extends HttpServlet {
@@ -21,27 +21,19 @@ public class LogIn extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
-        String username = null;
-        try {
-            username = CryptoSystemServer.decry(req.getParameter("username"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String password = null;
-        try {
-            password = CryptoSystemServer.decry(req.getParameter("password"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
         DatabaseConnection databaseConnection = new DatabaseConnection();
-        ResultSet rs = databaseConnection.GetResultFromSqlQuery("SELECT * FROM USERS WHERE ID=\"" + username + "\" AND PASSWORD=\"" + password + "\"");
+        ResultSet rs = databaseConnection.GetResultFromSqlQuery("SELECT password FROM USERS WHERE ID=\"" + username + "\"");
         try {
-            if (rs.next()){
+            if (rs.next() && CryptoSystemServer.decry(rs.getString(1)).equals(password)){
                 resp.addCookie(new Cookie("username",username));
                 resp.sendRedirect("home.jsp");
             }
             else resp.sendRedirect("login.jsp?status=wrong");
-        } catch (SQLException e) {
+        } catch (Exception e) {
             resp.sendRedirect("login.jsp?status=wrong");
             throw new RuntimeException(e);
         }
